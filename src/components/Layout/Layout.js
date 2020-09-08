3a71f3b743f9bda06170243cf5a258c72e450b7d
SaloniRokade <salonirokade119@gmail.com>
2020-09-08 14:34:39 +0530

    Fixes added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,8 +5,8 @@ import classes from '../Layout/Layout.module.css';
 const layout = (props) => {
 	return (
 		<div>
-			<Toolbar/>
-			<div>toolbar, sidedrawer, backdrop</div>
+			<Toolbar />
+			<div>sidedrawer, backdrop</div>
 			<main className={classes.Content}>
 				{props.children}
 			</main>
